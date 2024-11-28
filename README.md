2d19a686ad4e3baefae2b7c7a22d8939e96ebd53
sharmababita <git@stackblitz.com>
2024-11-28 10:09:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-m36z1r
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sharmababita/stackblitz-starters-m36z1r)
---
+++ script.js
@@ -0,0 +1,6 @@
+const convertButton = document.querySelector('#convertButton');
+
+convertButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log('Button is clicked!');
+});
